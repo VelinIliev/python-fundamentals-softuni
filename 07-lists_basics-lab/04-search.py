cc0ca8624120562bf486836ea8387f15d38b3b33
VelinIliev <veliniliev@abv.bg>
2022-08-23 10:16:33 +0300

    lists basics - lab

---
+++ 07-lists_basics-lab/04-search.py
@@ -0,0 +1,17 @@
+n = int(input())
+search_word = input()
+
+new_list = []
+
+for i in range(n):
+    new_entry = input()
+    new_list.append(new_entry)
+
+filtered_list = [word for word in new_list if search_word in word]
+
+# for word in new_list:
+#     if search_word in word:
+#         filtered_list.append(word)
+
+print(new_list)
+print(filtered_list)
