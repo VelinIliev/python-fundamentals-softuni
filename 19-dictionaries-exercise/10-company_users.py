1d63ab3bd6e9a0796f31ae8f295357bca556bdad
Velin Iliev <73158924+VelinIliev@users.noreply.github.com>
2022-08-28 16:07:46 +0300

    Dictionaries - Exercise

---
+++ 19-dictionaries-exercise/10-company_users.py
@@ -0,0 +1,24 @@
+companies = {}
+
+while True:
+    command = input().split(" -> ")
+    if command[0] == "End":
+        break
+    else:
+       company = command[0]
+       id = command[1]
+
+    if company not in companies:
+        companies[company] = []
+    if id in companies[company]:
+        continue
+    else:
+        companies[company].append(id)
+
+for key, value in companies.items():
+    company = key
+    employees = value
+    print(company)
+    for employee in employees:
+        print(f'-- {employee}')
+    # print(key, value)
