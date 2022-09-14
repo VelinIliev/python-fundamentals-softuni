e203857975ec28dbc57caea90208718ae86b03d0
VelinIliev <veliniliev@abv.bg>
2022-09-14 14:07:48 +0300

    Part One Interview:success JS Fundamentals:fundExam C# Fundamentals:fundPass Algorithms:fun end of contests C# Fundamentals=>fundPass=>Tanya=>350 Algorithms=>fun=>Tanya=>380 Part One Interview=>success=>Nikola=>120 Java Basics Exam=>wrong_pass=>Teo=>400 Part One Interview=>success=>Tanya=>220 OOP Advanced=>password123=>Kay=>231 C# Fundamentals=>fundPass=>Tanya=>250 C# Fundamentals=>fundPass=>Nikola=>200 JS Fundamentals=>fundExam=>Tanya=>400 end of submissions :

--- 20-dictionaries-more_exercise/01-ranking.py
+++ 20-dictionaries-more_exercise/01-ranking.py
@@ -1,71 +1,61 @@
 contests = {}
+users = {}
 while True:
     command = input()
     if command == "end of contests":
         break
     else:
-        entry = command.split(":")
-        if ':' in entry[0] or "=" in entry[0] or ">" in entry[0]:
-            continue
-        elif ':' in entry[1] or "=" in entry[1] or ">" in entry[1]:
-            continue
+        command = command.split(":")
+        contest = command[0]
+        password = command[1]
+        if ":" in contest or "=" in contest or ">" in contest:
+            pass
+        elif ":" in password or "=" in password or ">" in password:
+            pass
         else:
-            contest = entry[0]
-            password = entry[1]
             contests[contest] = password
 
-candidates = {}
 while True:
     command = input()
     if command == "end of submissions":
         break
     else:
-        entry = command.split("=>")
-        if ':' in entry[0] or "=" in entry[0] or ">" in entry[0]:
-            continue
-        elif ':' in entry[1] or "=" in entry[1] or ">" in entry[1]:
-            continue
-        elif ':' in entry[2] or "=" in entry[2] or ">" in entry[2]:
-            continue
-        elif 10000 < int(entry[3]) < 0:
-            continue
+        command = command.split("=>")
+        if ":" in command or "=" in command or ">" in command:
+            pass
         else:
-            contest = entry[0]
-            password = entry[1]
-            username = entry[2]
-            points = int(entry[3])
-            if contest in contests:
-                if password == contests[contest]:
-                    if username in candidates:
-                        if contest in candidates[username]:
-                            if candidates[username][contest] < points:
-                                candidates[username][contest] = points
-                        else:
-                            candidates[username][contest] = points
-                    else:
-                        candidates[username] = {contest: points}
-ranking = {}
+            contest = command[0]
+            password = command[1]
+            username = command[2]
+            points = int(command[3])
+            if 0 > points > 10000:
+                pass
+            else:
+                if contest in contests and password == contests.get(contest):
+                    if username not in users:
+                        users[username] = {}
+                    if contest in users[username]:
+                        if users[username][contest] < points:
+                            users[username][contest] = points
+                    elif contest not in users[username]:
+                        users[username][contest] = points
 
-for key, value in candidates.items():
-    name = key
+max_points = 0
+max_user = ""
+for name, courses in users.items():
     total_points = 0
-    for key1, value1 in value.items():
-        total_points += value1
-    ranking[name] = total_points
+    for points in courses.values():
+        points = points
+        total_points += points
+    if total_points > max_points:
+        max_points = total_points
+        max_user = name
 
-sorted_ranking = sorted(ranking.items(), key=lambda kv: kv[1])
-
-winner_name = max(sorted_ranking)[0]
-winner_points = max(sorted_ranking)[1]
-print(f'Best candidate is {winner_name} with total {winner_points} points.')
+print(f'Best candidate is {max_user} with total {max_points} points.')
 print("Ranking:")
+for key, value in sorted(users.items()):
+    print(key)
+    sorted_values = sorted(value.items(), key=lambda kv: kv[1], reverse=True)
+    for x in sorted_values:
+        print(f'#  {x[0]} -> {x[1]}')
 
-sorted_names = sorted(candidates)
-
-for name in sorted_names:
-    print(name)
-    sorted_courses = sorted(candidates[name].items(), key=lambda kv: kv[1], reverse=True)
-    for course in sorted_courses:
-        print(f'#  {course[0]} -> {course[1]}')
-
-# TODO: not ready
