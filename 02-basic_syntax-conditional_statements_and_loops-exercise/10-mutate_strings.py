1d6f9f07a46ec46fd8dd7a3cf43c9802af488432
Velin Iliev <73158924+VelinIliev@users.noreply.github.com>
2022-09-19 14:38:00 +0300

    refactor

--- 02-basic_syntax-conditional_statements_and_loops-exercise/10-mutate_strings.py
+++ 02-basic_syntax-conditional_statements_and_loops-exercise/10-mutate_strings.py
@@ -1,8 +1,8 @@
 string1 = input()
 string2 = input()
 
-for i in range(len(string1)):
-    if string1[i] != string2[i]:
-        string1 = string1[:i] + string2[i] + string1[i + 1:]
+for index in range(len(string1)):
+    if string1[index] != string2[index]:
+        string1 = string1[:index] + string2[index] + string1[index + 1:]
         print(string1)
 
