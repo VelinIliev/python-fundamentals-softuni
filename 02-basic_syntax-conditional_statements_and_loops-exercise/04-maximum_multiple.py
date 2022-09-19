8e28c8c7bce87c73c3c4c4a77ac4be48a2f08d25
Velin Iliev <73158924+VelinIliev@users.noreply.github.com>
2022-09-19 15:49:44 +0300

    refactor

--- 02-basic_syntax-conditional_statements_and_loops-exercise/04-maximum_multiple.py
+++ 02-basic_syntax-conditional_statements_and_loops-exercise/04-maximum_multiple.py
@@ -1,12 +1,7 @@
 divisor = int(input())
 boundary = int(input())
 
-max_number = 0
-
-for x in range(1, boundary + 1):
-    if x % divisor == 0:
-
-        if x > max_number:
-            max_number = x
-
-print(max_number)
+for number in range(boundary, divisor - 1, -1):
+    if number % divisor == 0:
+        print(number)
+        break
--- 02-basic_syntax-conditional_statements_and_loops-exercise/05-orders.py
+++ 02-basic_syntax-conditional_statements_and_loops-exercise/05-orders.py
@@ -6,7 +6,6 @@ for order in range(number_of_orders):
     price_per_capsule = float(input())
     days = int(input())
     capsules_needed = int(input())
-
     if 0.01 <= price_per_capsule <= 100 and 1 <= days <= 31 and 1 <= capsules_needed <= 2000:
         total_order = price_per_capsule * capsules_needed * days
         print(f'The price for the coffee is: ${total_order:.2f}')
