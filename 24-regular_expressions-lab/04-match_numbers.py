d3b66fd26944bcc4c346171db2a08534d726ae83
Velin Iliev <73158924+VelinIliev@users.noreply.github.com>
2022-09-21 16:56:32 +0300

    finished

--- 24-regular_expressions-lab/04-match_numbers.py
+++ 24-regular_expressions-lab/04-match_numbers.py
@@ -1,12 +1,8 @@
 import re
 
-pattern = r"(^|(?<=\s)-?)([0]|[1-9][0-9]*)(\.\d+)?($|(?=\s))"
+pattern = r"(^|(?<=\s))-?([0]|[1-9][0-9]*)(\.\d+)?($|(?=\s))"
 text = input()
-matches = re.findall(pattern, text)
+matches = re.finditer(pattern, text)
 
 for match in matches:
-    # print(match, end=" ")
-    # # print()
-    print("".join(match), end=" ")
-
-# TODO: 70/100
+    print(match.group(), end=" ")
