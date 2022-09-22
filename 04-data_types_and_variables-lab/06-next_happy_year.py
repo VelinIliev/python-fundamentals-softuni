30fb679ff0645b1f3b2b721810db2af0becc4ce7
Velin Iliev <73158924+VelinIliev@users.noreply.github.com>
2022-09-22 14:48:34 +0300

    new solution

--- 04-data_types_and_variables-lab/06-next_happy_year.py
+++ 04-data_types_and_variables-lab/06-next_happy_year.py
@@ -1,19 +1,30 @@
 year = int(input())
 
-next_happy_year = 0
-
 while True:
     year += 1
-    test_year = str(year)
-    year_as_list = [letter for letter in test_year]
-
-    non_repeated_digits = []
-    for digit in year_as_list:
-        if digit not in non_repeated_digits:
-            non_repeated_digits.append(digit)
-    if len(non_repeated_digits) == len(year_as_list):
-        next_happy_year = test_year
+    non_repeated_digits = set(digit for digit in str(year))
+    if len(non_repeated_digits) == len(str(year)):
         break
 
-print(next_happy_year)
+print(year)
+
+
+# year = int(input())
+#
+# next_happy_year = 0
+#
+# while True:
+#     year += 1
+#     test_year = str(year)
+#     year_as_list = [letter for letter in test_year]
+#
+#     non_repeated_digits = []
+#     for digit in year_as_list:
+#         if digit not in non_repeated_digits:
+#             non_repeated_digits.append(digit)
+#     if len(non_repeated_digits) == len(year_as_list):
+#         next_happy_year = test_year
+#         break
+#
+# print(next_happy_year)
 
--- 26-regular_expressions-more_exercises/02-softUni_bar_income.py
+++ 26-regular_expressions-more_exercises/02-softUni_bar_income.py
@@ -4,26 +4,28 @@ pattern_name = r'%([A-Z][a-z]+)%'
 pattern_product = r'<([\S]+)>'
 pattern_quantity = r'\|([0-9]+)\|'
 pattern_price = r'([0-9]+\.?[0-9]+)\$'
-
-command = input()
+# pattern = r'([\S]+)?%([A-Z][a-z]+)%<([\S]+)>([\S]+)?\|([0-9]+)\|([\S]+)?([0-9]+\.?[0-9]+)\$'
 grand_total = 0
 
+command = input()
 while command != "end of shift":
     text = command
     matches = []
+
     matches_name = re.findall(pattern_name, text)
+    matches_product = re.findall(pattern_product, text)
+    matches_quantity = re.findall(pattern_quantity, text)
+    matches_price = re.findall(pattern_price, text)
+
     if len(matches_name) > 0:
         matches.append(matches_name[0])
-    matches_product = re.findall(pattern_product, text)
     if len(matches_product) > 0:
         matches.append(matches_product[0])
-    matches_quantity = re.findall(pattern_quantity, text)
     if len(matches_quantity) > 0:
         matches.append(matches_quantity[0])
-    matches_price = re.findall(pattern_price,text)
     if len(matches_price) > 0:
         matches.append(matches_price[0])
-    # print(matches)
+
     if len(matches) == 4:
         name = matches[0]
         product = matches[1]
