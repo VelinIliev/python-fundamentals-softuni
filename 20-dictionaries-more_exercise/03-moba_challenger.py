ab77d818d32697510a7bae4e4cfc770b926133f2
VelinIliev <veliniliev@abv.bg>
2022-09-26 08:29:39 +0300

    05. Programming Fundamentals Mid Exam

--- 20-dictionaries-more_exercise/03-moba_challenger.py
+++ 20-dictionaries-more_exercise/03-moba_challenger.py
@@ -2,20 +2,55 @@ command = input()
 
 players = {}
 
-while " -> " in command:
-    entry = command.split(" -> ")
-    player = entry[0]
-    position = entry[1]
-    skill = int(entry[2])
-    if player not in players:
-        players[player] = {position: skill}
-    else:
-        if position in players[player] and players[player][position] < skill:
-            players[player][position] = skill
-        else:
-            players[player][position] = skill
-
+while "Season end" not in command:
+    if " -> " in command:
+        entry = command.split(" -> ")
+        player = entry[0]
+        position = entry[1]
+        skill = int(entry[2])
+        if 0 <= skill <= 1000:
+            if player not in players:
+                players[player] = {"skills": {position:skill},
+                                   'total': 0}
+            else:
+                if position in players[player]['skills'] and players[player]['skills'] [position] < skill:
+                    players[player]['skills'][position] = skill
+                else:
+                    players[player]['skills'][position] = skill
+    elif " vs " in command:
+        battle = command.split(" vs ")
+        first_player = battle[0]
+        second_player = battle[1]
+        if first_player in players and second_player in players:
+            for key, value in players[first_player]['skills'].items():
+                if key in players[second_player]['skills']:
+                    first_player_total = 0
+                    second_player_total = 0
+                    for points in players[first_player]['skills'].values():
+                        first_player_total += points
+                    for points in players[second_player]['skills'].values():
+                        second_player_total += points
+                    if first_player_total > second_player_total:
+                        players.pop(second_player)
+                    elif first_player_total < second_player_total:
+                        players.pop(first_player)
 
     command = input()
 
-print(players)
+for key, value in players.items():
+    total = 0
+    for points in value['skills'].values():
+        total += points
+    players[key]['total'] = total
+
+sorted_players = sorted(players.items(), key=lambda item: -item[1]["total"])
+# print(sorted_players[1])
+for x in sorted_players:
+    name = x[0]
+    total = x[1]["total"]
+    print(f'{name}: {total} skill')
+    sorted_skills = sorted(players[name]["skills"].items(), key=lambda kv: (-kv[1], kv[0]))
+    for y in sorted_skills:
+        print(f'- {y[0]} <::> {y[1]}')
+
+# TODO not redy, and 2 more cases in this chapter
--- 31-05-programming_fundamentals_mid_exam/03-inventory.py
+++ 31-05-programming_fundamentals_mid_exam/03-inventory.py
@@ -1,32 +1,45 @@
+# You will receive a journal with some collecting items,
+# separated with a comma and a space (", ").
 journal = input().split(", ")
+
+# After that, until receiving "Craft!" you will be
+# receiving different commands split by " - ":
 
 while True:
     command = input()
     if command == "Craft!":
-        print(", ".join(journal))
         break
     else:
         command = command.split(" - ")
-        action = command[0]
-        item = command[1]
-
+    action = command[0]
+    item = command[1]
+    #"Collect - {item}" - you should add the given
+    # item to your inventory. If the item already exists,
+    # you should skip this line.
     if action == "Collect":
         if item not in journal:
             journal.append(item)
+    #"Drop - {item}" - you should remove the item
+    # from your inventory if it exists.
     elif action == "Drop":
         if item in journal:
             journal.remove(item)
+    #"Combine Items - {old_item}:{new_item}" - you should
+    # check if the old item exists. If so, add the new
+    # item after the old one. Otherwise, ignore the command.
     elif action == "Combine Items":
         item = item.split(":")
         old_item = item[0]
         new_item = item[1]
         if old_item in journal:
-            for i in range(len(journal)):
-                if journal[i] == old_item:
-                    journal.append(new_item)
+            index = journal.index(old_item)
+            journal.insert(index + 1, new_item)
+    # "Renew – {item}" – if the given item exists,
+    # you should change its position and put it last in your inventory.
     elif action == "Renew":
         if item in journal:
             journal.remove(item)
             journal.append(item)
 
-# TODO: 70/100
+# After receiving "Craft!" print the items in your inventory, separated by ", ".
+print(", ".join(journal))
