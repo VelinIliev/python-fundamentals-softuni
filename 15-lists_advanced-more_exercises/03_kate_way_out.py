45786a8391ca2dd1ab42aa7fea2aba88db237290
VelinIliev <veliniliev@abv.bg>
2022-10-06 15:15:03 +0300

    solution found

--- 15-lists_advanced-more_exercises/03_kate_way_out.py
+++ 15-lists_advanced-more_exercises/03_kate_way_out.py
@@ -7,7 +7,6 @@ for _ in range(size):
     row = [x for x in row]
     maze.append(row)
 
-
 kate_position = []
 
 for row in range(len(maze)):
@@ -15,17 +14,13 @@ for row in range(len(maze)):
         if maze[row][column] == "k":
             kate_position = [row, column]
 
-# print(kate_position)
-moves = 0
-escape_fond = False
-
 row = kate_position[0]
 column = kate_position[1]
 
+escape_fond = False
+
 while True:
     if row - 1 < 0 or column - 1 < 0 or row + 1 > len(maze) - 1 or column + 1 > len(maze[0]) - 1:
-        moves += 1
-        # print(f"Kate got out in {moves} moves")
         escape_fond = True
         break
     if maze[row - 1][column] != " " and maze[row][column - 1] != " " and \
@@ -35,38 +30,24 @@ while True:
     # top
     if maze[row - 1][column] == " ":
         maze[row - 1][column] = "k"
-        row = row - 1
-        moves += 1
+        row -= 1
     # left
     elif maze[row][column - 1] == " ":
         maze[row][column - 1] = "k"
-        column = column - 1
-        moves += 1
+        column -= 1
     # bottom
     elif maze[row + 1][column] == " ":
         maze[row + 1][column] = "k"
-        row = row + 1
-        moves += 1
+        row += 1
     # right
     elif maze[row][column + 1] == " ":
         maze[row][column + 1] = "k"
-        column = column + 1
-        moves += 1
+        column += 1
 
 if escape_fond:
-    count = 0
+    count_moves = 0
     for x in maze:
-        count += x.count("k")
-        count += x.count(" ")
-    print(f'Kate got out in {count} moves')
-
-
-
-
-
-
-
-
-
-
+        count_moves += x.count("k")
+        count_moves += x.count(" ")
+    print(f'Kate got out in {count_moves} moves')
 
