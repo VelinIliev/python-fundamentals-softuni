99e5560b888d60c99c90af216c265b2bea1b7e75
VelinIliev <veliniliev@gmail.com>
2022-11-25 09:31:37 +0200

    finished

--- 26-regular_expressions-more_exercises/05-html_parser.py
+++ 26-regular_expressions-more_exercises/05-html_parser.py
@@ -1,19 +1,26 @@
 import re
 text = input()
-pattern_title = r'<title>([a-zA-Z\s]*)</title>[\w\W]*<body>([\w\W]*)</body>'
-matches = re.findall(pattern_title, text)
-title = matches[0][0]
-content = matches[0][1]
-
+pattern_title = r'<title>(.+)</title>'
+pattern_content = r'<body>(.+)</body>'
+matches_title = re.findall(pattern_title, text)
+matches_content = re.findall(pattern_content, text)
 tags = r'(<[^>]*>)'
 tabs = r'(\\n)|(\\t)'
+title = ''
+content = ''
+if matches_title and matches_content:
+    title = matches_title[0]
+    title = re.sub(tags, "", title)
+    title = re.sub(tabs, "", title)
+    while "  " in title:
+        title = title.replace("  ", " ")
 
-title = re.sub(tags, "", title)
-title = re.sub(tabs, "", title)
-content = re.sub(tags, "", content)
-content = re.sub(tabs, "", content)
+    content = matches_content[0]
+    content = re.sub(tags, "", content)
+    content = re.sub(tabs, "", content)
+    while "  " in content:
+        content = content.replace("  ", " ")
 
-print(f'Title: {title}')
-print(f'Content: {content}')
+    print(f'Title: {title}')
+    print(f'Content: {content}')
 
-# TODO: Not ready
